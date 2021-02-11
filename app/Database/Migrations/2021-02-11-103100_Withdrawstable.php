56013ce81e19cc557c31a60b855c5d6c8175a714
Joseph <talktojoegee@gmail.com>
2021-02-11 11:34:04 +0100

    Withdraw migration file

---
+++ app/Database/Migrations/2021-02-11-103100_Withdrawstable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Withdrawstable extends Migration
+{
+	public function up()
+	{
+		//
+		$this->db->disableForeignKeyChecks();
+		$fields = [
+			'cart' => [
+                    'type' => 'INT',
+                    'null' => 'true',
+                    'default'=>0
+
+                ],
+                'paid_back' => [
+                    'type' => 'INT',
+                    'null' => 'true',
+                    'default'=>0
+
+                ],
+                'disburse' => [
+                    'type' => 'INT',
+                    'null' => 'true',
+                    'default'=>0
+
+                ],
+                'scheduled' => [
+                    'type' => 'INT',
+                    'null' => 'true',
+                    'default'=>0
+
+                ],
+                'disburse_date' => [
+                    'type' => 'DATETIME',
+                    'null' => 'true',
+                    'default'=>0
+
+                ],
+			
+			
+		];
+		$this->forge->addColumn('withdraws', $fields);
+	}
+
+	public function down()
+	{
+		//
+	}
+}
